4ef58cc683f0203ae9063bfaf126dba6640b0160
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2023-03-09 16:40:23 +0100

    feat: add play, pause and reset functionality

--- src/main/java/com/jan0/music_player/Controller.java
+++ src/main/java/com/jan0/music_player/Controller.java
@@ -1,4 +1,93 @@
 package com.jan0.music_player;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Controller implements Initializable {
+    @FXML
+    private Pane pane;
+    @FXML
+    private Button playButton, pauseButton, resetButton, prevButton, nextButton;
+    @FXML
+    private Label songLabel;
+    @FXML
+    private ComboBox<String> speedBox;
+    @FXML
+    private Slider volumeSlider;
+    @FXML
+    private ProgressBar songProgressBar;
+
+    private Media media;
+    private MediaPlayer mediaPlayer;
+
+    private File directory;
+    private File[] files;
+    private ArrayList<File> songs;
+    private int songNumber;
+    private int[] speeds = {25, 50, 75, 100, 125, 150, 175, 200};
+
+    private Timer timer;
+    private TimerTask task;
+    private boolean running;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        songs = new ArrayList<>();
+        directory = new File("music");
+        files = directory.listFiles();
+        if (files != null) {
+            for (File file: files) {
+                songs.add(file);
+            }
+        }
+        media = new Media(songs.get(songNumber).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+
+        songLabel.setText(songs.get(songNumber).getName());
+    }
+
+    public void playMedia() {
+        mediaPlayer.play();
+    }
+
+    public void pauseMedia() {
+        mediaPlayer.pause();
+    }
+
+    public void resetMedia() {
+        mediaPlayer.seek(Duration.seconds(0));
+    }
+
+    public void prevMedia() {
+
+    }
+
+    public void nextMedia() {
+
+    }
+
+    public void changeSpeed(ActionEvent event) {
+
+    }
+
+    public void beginTimer() {
+
+    }
+
+    public void cancelTimer() {
+
+    }
 }
